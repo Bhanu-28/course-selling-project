0e1fc4166a8d6f6f3cbd58bbda071823f36fc173
Bhanu-28 <bhanupradeep123456@gmail.com>
2025-08-27 22:19:38 +0530

    Step-8  Signup End point zod validation and save hashed password in db

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const { userRouter } = require('./routes/user')
 const { courseRouter } = require('./routes/courses')
 const { adminRouter } = require('./routes/admin')
 const mongoose = require("mongoose")
-
 const app = express()
 const port = 3000
+
+app.use(express.json())
 
 app.get('/',(req,res) =>{
     res.send("Hello world")
--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,75 @@ const express = require("express")
 const userRouter = express.Router()
 
 
+// Import the dependencies
 
+const bcrypt = require("bcrypt")
+const zod = require("zod")
+const { userModel } = require("../db")
+
+
+
+userRouter.post('/signup',async function(req,res) {
+
+
+    // Input validation.
+    const requiredBody = zod.object({
+
+        email : zod.email(),
+        password : zod.string().min(5).max(100),
+        firstName : zod.string().min(3).max(20),
+        lastName : zod.string().min(3).max(20)
+    }
+
+)
+
+    // Parse the request body.
+    const parseDataWithSuccess = requiredBody.safeParse(req.body);
+
+    // Now checking and warning whether the user/admin has given some invalid input or not
+
+    if(!parseDataWithSuccess.success){
+        res.status(400).json({
+            msg : "Invalid Input Given",
+            error: parseDataWithSuccess.error.issues
+        })
+        return
+    }
+
+    const {email, password, firstName, lastName} = req.body;
+
+    // Hashing the password so plain password is not stored in the database.
+
+    //error handle with try catch.
+
+
+    let errorFound = false;
+
+
+    try{
+        const hashedPassword  = await bcrypt.hash(password,10);
+
+        await userModel.create({
+            email : email,
+            password : hashedPassword,
+            firstName : firstName,
+            lastName : lastName
+        })
+    }catch(e){
+        res.status(400).json({
+            msg : "Email entered Already Exists in the Database!"
+        })
+
+        errorFound = true;
+    }
+
+    if(!errorFound){
+        res.json({
+            msg: `${firstName} Successfully SignedUP to the database, as USER!`
+        })
+    }
+
+})
 
 userRouter.post('/login',(req,res)=>{
     res.json({
@@ -11,11 +79,6 @@ userRouter.post('/login',(req,res)=>{
     })
 })
 
-userRouter.post('/signup',(req,res)=>{
-    res.json({
-        message : "User Signup "
-    })
-})
 
 userRouter.post('/purchase',(req,res)=>{
     res.json({
